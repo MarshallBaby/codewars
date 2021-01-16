520deee01556dcfcdfd78e05919a40c4dafd1de4
MarshallBaby <aleksey.yatsevich@gmail.com>
2021-01-16 18:00:29 +0300

    string2

--- stringbreakers.js
+++ stringbreakers.js
@@ -1,14 +1,20 @@
 function stringBreakers(n, string){
-    let count = 0;
-    return string.split(' ').join("").split("").map((e) => {
-        count++;
-        if(count == n){
-            count = 0;
-            return e += "\n"
-        }else{
-            return e
-        }
-    }).join("");
-}
-
-console.log(stringBreakers(5, 'This is an example string'));
+      let count = -1;
+      let finStr = string.split(" ").join("").split("");
+      if(n == 1){
+        return finStr.map((e) => {return e += "\n"}).join("")
+    }
+      let i = 0;
+      while(i < finStr.length){
+          count++;
+          if(count == n - 1 && i !=0 && i != finStr.length - 1){
+              finStr[i] +=  "\n";
+              i++;
+              count = -1;
+          }else{        
+              i++;
+          }
+      }
+      return finStr.join("");
+  }
+console.log(stringBreakers(1, '8uzjrqvamx2y5oluhe81xnahscsyq6pvrn77vmnzbkwboam3c'));
